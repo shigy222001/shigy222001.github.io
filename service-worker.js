26f1329599535b5c19644b1ab4d64db05354da7b
shigy222001 <49425917+shigy222001@users.noreply.github.com>
2019-04-10 13:30:40 +0900

    push test

--- service-worker.js
+++ service-worker.js
@@ -8,3 +8,31 @@ self.addEventListener('activate', function(e) {
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
 self.addEventListener('fetch', function(event) {});
+
+// Push通知
+self.addEventListener("push", function(event) {
+  event.waitUntil(
+    self.registration.pushManager.getSubscription()
+      .then(function(subscription) {
+        if (subscription) {
+          return subscription.endpoint
+        }
+        throw new Error('User not subscribed')
+    })
+    .then(function(res) {
+      return fetch('/notifications.json')
+    })
+    .then(function(res) {
+      if (res.status === 200) {
+        return res.json()
+      }
+      throw new Error('notification api response error')
+    })
+    .then(function(res) {
+      return self.registration.showNotification(res.title, {
+        icon: '/icon-256.png',
+        body: res.body
+      })
+    })
+  )
+})
