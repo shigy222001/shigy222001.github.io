612e45a2870fc931ba338cc934d884026372ded9
shigy222001 <49425917+shigy222001@users.noreply.github.com>
2019-12-25 12:36:16 +0900

    MagicLeap

---
+++ PWA_OLD/serviceworker0.js
@@ -0,0 +1,29 @@
+let baseURL = '/';
+
+self.addEventListener('push', event => {
+    let json = event.data.json();
+
+    baseURL = json.data.url;
+
+    event.waitUntil(
+        self.registration.showNotification(json.notification.title, {
+            'body': json.notification.body,
+            'tag': 'request',
+//            'actions': JSON.parse(json.data.action)
+            'actions': [{"action": "select-a","title": "はい"},
+              {"action": "select-b","title": "いいえ"}]
+        })
+    );
+});
+
+self.addEventListener('notificationclick', event => {
+    if (event.action === 'select-a') {
+        self.clients.openWindow('https://ja.wikipedia.org/wiki/1');
+    } else if (event.action === 'select-b') {
+        self.clients.openWindow('https://ja.wikipedia.org/wiki/2');
+    } else {
+        self.clients.openWindow(baseURL);
+    }
+
+    event.notification.close();
+});
